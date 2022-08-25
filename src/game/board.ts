5f867e250a7c2b31ed9ebadef1cdeba3230b52a4
secondary-smiles <shavkinder@gmail.com>
2022-08-24 22:53:27 -0400

    Add proper Y axis spacing

--- src/game/board.ts
+++ src/game/board.ts
@@ -5,9 +5,13 @@ async function initBoard(x: number, y: number, board: HTMLDivElement) {
 async function initGrid(xnum: number, ynum: number, board: HTMLDivElement) {
     for (let y = 0; y < ynum; y++) {
         board.innerHTML += `<div class='row' id='row-${y}'><div>`;
+        let row = document.querySelector<HTMLDivElement>(`#row-${y}`)!;
+
+        row.style.height = `${board.clientHeight/ynum}px`
+
         for (let x = 0; x < xnum; x++) {
-            const row = document.querySelector(`#row-${y}`)!;
             row.innerHTML += `<div class='grid' id='grid-${x}'><div>`;
+            const grid = document.querySelector<HTMLDivElement>(`#grid-${x}`)!;
         }
     }
 }
